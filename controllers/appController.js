36142f274ba156a190e4a5525fa2752e6d8df30e
lameesad <lameesaboudarwish@gmail.com>
2022-03-08 00:11:40 +0200

    cleaning code

--- controllers/appController.js
+++ controllers/appController.js
@@ -5,67 +5,6 @@ import mongoose from "mongoose";
 import moment from "moment";
 import Job from "../models/Job.js";
 
-// const applyJob = async (req, res) => {
-//   const user = req.user;
-//   if (user.type == "admin") {
-//     res.status(401).json({
-//       message: "You don't have permissions to apply for a job",
-//     });
-//     return;
-//   }
-//   const data = req.body;
-//   const jobId = req.params.id;
-
-//   // check whether applied previously
-//   // find a job
-//   // store the data in applications
-
-//   Application.findOne({
-//     userId: user._id,
-//     jobId: jobId,
-//     status: {
-//       $nin: ["deleted", "accepted", "cancelled"],
-//     },
-//   }).then((appliedApplication) => {
-//     console.log(appliedApplication);
-//     if (appliedApplication !== null) {
-//       res.status(400).json({
-//         message: "You have already applied for this job",
-//       });
-//       return;
-//     }
-
-//     Job.findOne({ _id: jobId })
-//       .then((job) => {
-//         if (job === null) {
-//           res.status(404).json({
-//             message: "Job does not exist",
-//           });
-//           return;
-//         }
-//         const application = new Application({
-//           userId: user._id,
-//           jobId: job._id,
-//           status: "applied",
-//         });
-//         application
-//           .save()
-//           .then(() => {
-//             res.json({
-//               message: "Job application successful",
-//             });
-//           })
-
-//           .catch((err) => {
-//             res.status(400).json(err);
-//           });
-//       })
-//       .catch((err) => {
-//         res.status(400).json(err);
-//       });
-//   });
-// };
-
 const applyJob = async (req, res) => {
   const { job: jobId } = req.body;
 
@@ -86,6 +25,7 @@ const applyJob = async (req, res) => {
 
   req.body.user = req.user.userId;
   const application = await Application.create(req.body);
+  console.log(application);
   res.status(StatusCodes.CREATED).json({ application });
 };
 
